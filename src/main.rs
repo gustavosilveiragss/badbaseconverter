69290b8ff9498ff2b33d89a6784e89afa79545a9
Gustavo Silveira <gustavosilveira@pop-os.localdomain>
2023-03-19 10:42:48 -0300

    added pattern

--- src/main.rs
+++ src/main.rs
@@ -30,29 +30,31 @@ struct Cli {
     num: String,
 }
 
-fn binary_to_decimal() -> String {
+fn binary_to_decimal(args: Cli) {
     let bin = &args.num.trim().to_string();
 
     // check if input consists only of 1 and 0
     if !bin.chars().all(|x| "01".contains(x)) {
         println!("INVALID CHARACTERS. TRY AGAIN:");
-        get_binary();
+        binary_to_decimal(args);
     }
 
-    return bin.clone();
+    bintodec::bi_to_dec(bin.clone());
+}
+
+fn decimal_to_binary(args: Cli) {
+    
 }
 
 use anyhow::Result;
 fn main() -> Result<()> {
     let args = Cli::parse();
 
-    match &args.pattern {
-        "bitodec" | "btc" | "bintodec" | "binarytodecimal" => binary_to_decimal();  
+    match args.pattern.as_str() {
+        "bitodec" | "btd" | "bintodec" | "binarytodecimal" => binary_to_decimal(args),
+        "dectobi" | "dtb" | "dectobin" | "decimaltobinary" => decimal_to_binary(args),
+        &_ => println!("MUST INPUT CONVERSION PATTERN MATCHING OPTIONS")
     }
-
-    let bin = get_binary();
-
-    bintodec::bi_to_dec(bin);
 
     Ok(())
 }
