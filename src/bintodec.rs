0503ecfe716c4c2e2b49c268a7b0471edb137ba7
James ABIB <james.bidule@gmail.com>
2023-11-17 11:02:49 +0100

    refactor(bintodec): using fold instead of a for
    Refactored the `bi_to_dec` function in `bintodec.rs` to improve readability and performance. Replaced a for loop with a fold operation to calculate the decimal value of a binary string.

--- src/bintodec.rs
+++ src/bintodec.rs
@@ -1,15 +1,11 @@
 pub fn bi_to_dec(bin: String) -> String {
-    let bytes = bin.as_bytes();
-
-    let mut sum: usize = 0;
-
-    for (i, &char) in bytes.iter().enumerate() {
-        let inverted_pos = bin.chars().count() - (i + 1);
-
-        if char == b'1' {
-            sum += 2usize.pow(inverted_pos as u32);
-        }
-    }
+    let sum = bin.as_bytes()
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (i, &b)| {
+            let inverted_pos = bin.len() - i - 1;
+            acc + if b == b'1' { 2usize.pow(inverted_pos as u32) } else { 0 }
+        });
 
     println!("the binary \"{bin}\" in decimal is: {sum}");
 
